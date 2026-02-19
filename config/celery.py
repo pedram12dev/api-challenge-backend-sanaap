8a4228cecb6300de32d61adffc4dd8c824d2b69c
pedram <pedram.najafi73@gmail.com>
2026-02-19 13:30:23 +0330

    fix celery tasks settings

--- config/celery.py
+++ config/celery.py
@@ -4,5 +4,5 @@ from celery import Celery
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.django.local')
 
 celery = Celery('config')
-celery.config_from_object('django.conf:django', namespace='CELERY')
+celery.config_from_object('django.conf:settings', namespace='CELERY')
 celery.autodiscover_tasks()
